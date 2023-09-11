d467d3d284878dcef5b6d7ae6d8f54675c3fe5a1
Lidwine Carême <lidwine.careme@gmail.com>
2023-09-11 10:37:57 +0200

    exo 3.2 done

---
+++ 3.2/3.2.js
@@ -0,0 +1,10 @@
+let arr = [1, 2, 3, 4, 5];
+let amount = arr.reduce((acc, valeur) => acc + valeur, 0);
+let average = amount/ arr.length;
+console.log(average);
+
+
+let num = [100, 101, 102];
+let add = num.reduce((acc, valeur) => acc + valeur, 0);
+let av = add / num.length;
+console.log(av);
