29951fdec93927371e3b56c5a6ce08e94a7df36a
Lidwine Carême <lidwine.careme@gmail.com>
2023-09-11 09:47:12 +0200

    ex 3.0 done

---
+++ 3.0/3.0.js
@@ -0,0 +1,5 @@
+let arr = ["Alexandre", "Antoine", "Bastien", "Carole", "Dorian", "Elisabeth", "Elodie", "Justin", "Justine F", "Justine L", "Kimi", "Layla", "Lidwine", "Lucas", "Marie", "Mathias", "Okly", "Pierre", "Robin", "Rosalie", "Stephane", "Tim", "Thomas", "Valentin", "Alexandre V", "Virginie"];
+
+for (let elem of arr) {
+    console.log(elem);
+}
