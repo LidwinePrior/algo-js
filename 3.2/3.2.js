ee3938799d3709002de3542492d963c8bfe94da3
Lidwine Carême <lidwine.careme@gmail.com>
2023-09-11 10:54:36 +0200

    exo 3.2 done

--- 3.2/3.2.js
+++ 3.2/3.2.js
@@ -1,10 +1,23 @@
 let arr = [1, 2, 3, 4, 5];
-let amount = arr.reduce((acc, valeur) => acc + valeur, 0);
-let average = amount/ arr.length;
+let amount = 0;
+for(let i= 0; i< arr.length; i++) {
+    amount += arr[i];
+}
+let average = amount / arr.length;
 console.log(average);
 
 
+
+
 let num = [100, 101, 102];
-let add = num.reduce((acc, valeur) => acc + valeur, 0);
+let add = 0;
+for (let a= 0; a< num.length; a++) {
+    add += num[a];
+}
 let av = add / num.length;
 console.log(av);
+
+// let num = [100, 101, 102];
+// let add = num.reduce((acc, valeur) => acc + valeur, 0);
+// let av = add / num.length;
+// console.log(av);
