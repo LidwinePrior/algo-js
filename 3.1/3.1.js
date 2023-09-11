5d501b25cebb26ac5a13b97aaadfc76affa0a31b
Lidwine Carême <lidwine.careme@gmail.com>
2023-09-11 10:16:49 +0200

    exo 3.1 done

---
+++ 3.1/3.1.js
@@ -0,0 +1,12 @@
+let arr = [1, 2, 3, 4, 5];
+let amount = 0;
+for( let i= 0; i < arr.length; i++) {
+    amount += arr[i];
+} console.log(amount);
+
+
+let num = [100, 101, 102];
+let add = 0;
+for(let a= 0; a < num.length; a++) {
+    add += num[a];
+} console.log(add);
