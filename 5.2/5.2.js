b1b073842a0715439901418c68c83560ff803672
Lidwine Carême <lidwine.careme@gmail.com>
2023-09-12 16:58:03 +0200

    add explanations

--- 5.2/5.2.js
+++ 5.2/5.2.js
@@ -2,16 +2,16 @@
 
 
 
-function shuffle(actors) {    //fonction qui mélange les noms des acteurs rentrés par l'utilisateur
-    for (let i = actors.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [actors[i], actors[j]] = [actors[j], actors[i]];
+function shuffle(actors) {    //fonction qui prend un tableau d'acteurs en entrée
+    for (let i = actors.length - 1; i > 0; i--) {  // boucle for qui commence à la fin du tableau des acteurs et se déplace vers l'arrière jusqu'a l'index 0. Elle permettra de mélanger les acteurs
+      const j = Math.floor(Math.random() * (i + 1));  // a chaque itérationde la boucle, cette ligne génère un nombre aléatoire j entre o et i inclus. Cela  servira à choisir un acteur aléatoire pour l'échanger avce l'acteur à l'index i
+      [actors[i], actors[j]] = [actors[j], actors[i]];   // echange les éléments actors [i] et [j]
     }
   }
 
-  function randomizeCast(tvSerie) {
-    const actors = tvSerie.actors;
-    shuffle(actors);
+  function randomizeCast(tvSerie) {  //fonction  qui prend un objet tvSerie et la stock e dans une variable
+    const actors = tvSerie.actors;   //extrait la liste des acteurs de l'objet tvSerie et la stock dans la variable actors
+    shuffle(actors);   //appele la fonction shuffle que j'ai déclaré plus haut pour melanger la liste
   }
  
   
@@ -26,6 +26,6 @@ function shuffle(actors) {    //fonction qui mélange les noms des acteurs rentr
     prodYear: prodYear,
     actors: actors,
   };
- 
-  randomizeCast(tvSerie);
-  console.log(tvSerie.actors);
+  console.log("distribution normale: " + actors)
+  randomizeCast(tvSerie); 
+  console.log("distribution aléatoire des acteurs: " + tvSerie.actors);
