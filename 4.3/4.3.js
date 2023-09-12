35b2deafcbe25784eb77c173e1df652a6d251a9b
Lidwine Carême <lidwine.careme@gmail.com>
2023-09-12 09:14:57 +0200

    add explanations

--- 4.3/4.3.js
+++ 4.3/4.3.js
@@ -1,12 +1,12 @@
-// 4.3 
+// 4.3 créé un programme  qui demande le nombre de nombres random et l'affiche 
 
-function rand10() {
+function rand10() {   //fonction qui génère un nombre entier aléatoire entre 1 et 10
     return Math.floor(Math.random() *10) +1;  
 }
 
-function multiRand(n) {
+function multiRand(n) {   
     let result = [];
-    for (let i = 0; i< n; i++) {
+    for (let i = 0; i< n; i++) {     // boucle for parcours rand10 et ajoute les éléments a son tableau
         result.push(rand10());
     }
     return result;
